c560ddf79e4652ee8f564be827cf3a739fd76a96
이지우 <20241257@sungshin.ac.kr>
2026-05-28 14:17:41 +0900

    feat: Google Vision OCR 연동 및 이미지 업로드 구현

--- routes/medicineRoutes.js
+++ routes/medicineRoutes.js
@@ -1,5 +1,25 @@
+const {
+  extractTextFromImage
+} = require('../services/ocrService');
+
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+
+    cb(null, Date.now() + ext);
+  }
+});
+
+const upload = multer({ storage });
 
 router.get('/add', (req, res) => {
   res.render('medicines/register-ocr', {
@@ -18,5 +38,33 @@ router.get('/manual', (req, res) => {
     pageCss: 'medicine'
   });
 });
+
+router.post(
+  '/ocr',
+  upload.single('medicineImage'),
+  async (req, res) => {
+
+    try {
+
+      console.log(req.file);
+
+      const extractedText =
+        await extractTextFromImage(req.file.path);
+
+      console.log(extractedText);
+
+      res.send(`
+        <h2>OCR 결과</h2>
+        <pre>${extractedText}</pre>
+      `);
+
+    } catch (error) {
+
+      console.error(error);
+
+      res.status(500).send('OCR 처리 실패');
+    }
+  }
+);
 
 module.exports = router;
