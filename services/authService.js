9cb76f92f067e9ed091eedd0bc19c801d5303f47
이고은 <20231152@sungshin.ac.kr>
2026-05-31 20:39:00 +0900

    develop 병합 후 인증 패키지 반영

--- services/authService.js
+++ services/authService.js
@@ -3,12 +3,9 @@ const db = require("../config/db");
 exports.findOrCreateUser = async (profile) => {
   return new Promise((resolve, reject) => {
     const googleId = profile.id;
-    const email = profile.emails[0].value;
-    const name = profile.displayName;
-    const profileImage = profile.photos[0].value;
+    const name = profile.displayName || "Google 사용자";
 
-    const findUserQuery =
-      "SELECT * FROM `USER` WHERE google_id = ?";
+    const findUserQuery = "SELECT * FROM `USER` WHERE google_id = ?";
 
     db.query(findUserQuery, [googleId], (err, results) => {
       if (err) {
@@ -21,28 +18,28 @@ exports.findOrCreateUser = async (profile) => {
 
       const insertUserQuery = `
         INSERT INTO \`USER\`
-        (google_id, email, name, profile_image, provider)
-        VALUES (?, ?, ?, ?, 'google')
+        (
+          google_id,
+          provider,
+          name,
+          password
+        )
+        VALUES (?, 'google', ?, 'GOOGLE_LOGIN')
       `;
 
-      db.query(
-        insertUserQuery,
-        [googleId, email, name, profileImage],
-        (err, result) => {
-          if (err) {
-            return reject(err);
-          }
-
-          resolve({
-            user_id: result.insertId,
-            google_id: googleId,
-            email,
-            name,
-            profile_image: profileImage,
-            provider: "google",
-          });
+      db.query(insertUserQuery, [googleId, name], (err, result) => {
+        if (err) {
+          return reject(err);
         }
-      );
+
+        resolve({
+          user_id: result.insertId,
+          google_id: googleId,
+          provider: "google",
+          name,
+          password: "GOOGLE_LOGIN",
+        });
+      });
     });
   });
 };
